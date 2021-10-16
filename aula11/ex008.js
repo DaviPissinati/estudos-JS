99d61d5c84529b326f34181d6c4574862807a278
Your NameRuanPissinati <ruanpissinati@MacBook-Pro-de-Ruan.local>
2021-10-16 16:31:33 -0300

    first commit

---
+++ aula11/ex008.js
@@ -0,0 +1,12 @@
+
+
+
+let vel = 70.5;
+console.log(`a velocidade do seu carro e ${vel}Km/h`);
+
+if(vel > 60){
+    console.log("voce esta a cima da velocidade multado")
+}
+
+
+console.log("Dirija sempre de cinto de segurança");
