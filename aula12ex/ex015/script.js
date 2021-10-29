a8277cf16294abb1823c5ae8df7c364498e98ab5
Your NameRuanPissinati <ruanpissinati@MacBook-Pro-de-Ruan.local>
2021-10-29 08:08:42 -0300

    aula 13

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -4,6 +4,47 @@ function verificar() {
     var fano = document.getElementById('txtano');
     var res = document.querySelector('#res');
 
+    if(fano.value.length == 0 || Number (fano.value > ano)){
+        alert('[ERRO] verifique os dados novamente')
+    }else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ""
+        var img = document.createElement('img')
+        img.setAttribute('id' , 'foto')
+
+        if (fsex[0].checked) {
+            genero = "Masculino"
+
+            if (idade >= 0 && idade <= 10) {
+                img.setAttribute('src', 'foto-bebe-h.png')
+            } else if(idade < 21){
+                img.setAttribute('src', 'foto-jovem-h.png')
+            }else if(idade < 50){
+                img.setAttribute('src', 'foto-adulto-h.png')
+            }else {
+                img.setAttribute('src', 'foto-idoso-h.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'feminino'
+
+            if (idade >= 0 && idade <= 10) {
+                img.setAttribute('src', 'foto-bebe-m.png')
+            } else if(idade < 21){
+                img.setAttribute('src', 'foto-jovem-m.png')
+            }else if(idade < 50){
+                img.setAttribute('src', 'foto-adulto-m.png')
+            }else {
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }
+
+        }
+        
+        res.style.textAlign = 'center'
+        res.innerHTML = `seu genero e ${genero} e sua idade e ${idade}`
+        res.appendChild(img)
+        
+    }
     
     
 }
---
+++ aula12ex/test-copiar-e-colar/script.js
@@ -0,0 +1,15 @@
+var email = document.getElementById('email')
+    
+
+
+function copiar(copiar) {
+    
+    var copiar = email.value
+
+    return
+}
+
+
+function colar(colar){
+    var colar = email.value
+}
